d1faa5abc2ffecb78d9e6384a99a0e77702ed3c1
Nam Nguyen <nnam1472@gmail.com>
2016-11-02 17:16:10 +0700

    Init Project

---
+++ test/src/test.java
@@ -0,0 +1,20 @@
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+
+public class test extends JFrame {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		test f = new test();
+		f.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		f.setVisible(true);
+		f.setPreferredSize(new Dimension(500, 500));
+		
+	}
+
+}
